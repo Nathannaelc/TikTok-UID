4c9950505627f660bc68f6d799809c80fa95e34c
Nathannaelc <65099536+Nathannaelc@users.noreply.github.com>
2021-03-23 22:50:48 +0800

    Add user info grabber and comments

--- start.py
+++ start.py
@@ -1,16 +1,37 @@
 import re
 import requests
 
-###!/bin/bash
-##echo Please enter shared video url
-##read link
+#ask for user input
 short_link = input("Please enter shared video url : ")
-##user_ID=`curl -s $link | sed 's/.*user_id=\([0-9]*\).*/\1/' | awk '!seen[$0]++'`
+
+#Unshorten the link
 r = requests.get(short_link, allow_redirects=False)
 long_link = (r.headers['location'])
+
+#grab the user_id
 m = re.search('&user_id=(\d+)', long_link, re.IGNORECASE)
 uid = m.group(1)
-##echo "User ID :" $user_ID
-##echo "Link    : https://tiktok.com/@"$user_ID
-print("User ID :", uid)
-print("Link    : https://tiktok.com/@" + uid)
+
+uid_link = ("https://tiktok.com/@" + uid)
+
+print("Grabbing user info.... Please wait")
+
+#Get User Info ( Username, name, following, followers, likes, description )
+from bs4 import BeautifulSoup
+r2 = requests.get(uid_link)
+soup = BeautifulSoup(r2.content, 'html.parser')
+username = soup.find_all("h2", class_="jsx-2997938848 share-title")[0].text
+name = soup.find_all("h1", class_="share-sub-title")[0].text
+following = soup.find_all("strong", title="Following")[0].text
+followers = soup.find_all("strong", title="Followers")[0].text
+likes = soup.find_all("strong", title="Likes")[0].text
+description = soup.find_all("h2", class_="share-desc mt10")[0].text
+
+#print datas
+print("User ID   :", uid)
+print("Username  :", username)
+print("Name      :", name)
+print("Following :", following)
+print("Followers :", followers)
+print("Likes     :", likes)
+print("Bio       :", description)
